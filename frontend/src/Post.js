0c0026c49683a3a3c2283e4d2aacb6e85c80eb34
joaozati <jzati@mitsidi.com>
2023-05-19 17:57:09 -0300

    Adding comments in Post element

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -6,26 +6,42 @@ const BASE_URL = 'http://localhost:8000'
 function Post({ post }) {
 
     const [imageUrl, setImageUrl] = useState('');
+    const [comments, setComments] = useState([]);
+    
     
     useEffect(() => {
-        console.log("Building Post")
-        console.log(post)
-
         if (post.image_url_type == "absolute") {
-            console.log("post image url")
-            console.log(post.image_url)
             setImageUrl(post.image_url)
         } else {
             setImageUrl(BASE_URL + post.image_url)
         }
     }, []);
 
-    console.log("image URL:  ")
-    console.log(imageUrl)
+    useEffect(() => {
+        console.log("UseEffect2: ")
+        var commentsElements = [] 
+        
+        for (let i = 0; i < post.comments.length; i++) {
+            commentsElements.push(
+                <p>
+                    <strong>{post.comments[i].username}: </strong> {post.comments[i].text}
+                </p>
+            )
+        }
+
+        setComments(commentsElements)
+        console.log("comments: ")
+        console.log(comments)
+    }, []);
 
     return (
         <div className='post'>
             <img className='post_image' src={imageUrl}></img>
+
+            <h4 className='post_text'>{post.caption}</h4>
+            <div className="post_comments">
+                {comments}
+            </div>
         </div>
     );
 }
