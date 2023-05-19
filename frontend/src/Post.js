3fae6803aeaacf556d9b9f5643753e80530cb9ec
joaozati <jzati@mitsidi.com>
2023-05-19 18:18:49 -0300

    Adicionando condicional para rendeizar comentario

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './Post.css'
 
 const BASE_URL = 'http://localhost:8000'
@@ -7,8 +7,8 @@ function Post({ post }) {
 
     const [imageUrl, setImageUrl] = useState('');
     const [comments, setComments] = useState([]);
-    
-    
+
+
     useEffect(() => {
         if (post.image_url_type == "absolute") {
             setImageUrl(post.image_url)
@@ -18,30 +18,36 @@ function Post({ post }) {
     }, []);
 
     useEffect(() => {
-        console.log("UseEffect2: ")
-        var commentsElements = [] 
-        
-        for (let i = 0; i < post.comments.length; i++) {
-            commentsElements.push(
-                <p>
-                    <strong>{post.comments[i].username}: </strong> {post.comments[i].text}
+        const generateCommentElements = () => {
+            return post.comments.map((comment, index) => (
+                <p key={index}>
+                    <strong>{comment.username}: </strong> {comment.text}
                 </p>
-            )
-        }
+            ));
+        };
 
-        setComments(commentsElements)
-        console.log("comments: ")
-        console.log(comments)
-    }, []);
+        const renderComments = () => {
+            const commentElements = generateCommentElements();
+
+            if (commentElements.length > 0) {
+                return (
+                    <div className="post_comments">
+                        {commentElements}
+                    </div>
+                );
+            }
+
+            return null;
+        };
+
+        setComments(renderComments());
+    }, [post.comments]);
 
     return (
         <div className='post'>
             <img className='post_image' src={imageUrl}></img>
 
-            <h4 className='post_text'>{post.caption}</h4>
-            <div className="post_comments">
-                {comments}
-            </div>
+            {comments}
         </div>
     );
 }
